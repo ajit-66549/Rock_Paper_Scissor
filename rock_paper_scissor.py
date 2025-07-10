b88ab17586951431f86e60c52a77d625b53bae39
Ajit Chaudhary <ajitchaudhary6793@gmail.com>
2025-07-10 00:53:44 -0500

    Second commit

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -24,54 +24,68 @@ def is_valid_input(user_choice, choices):
     return user_choice in choices
 
 def winner(random_choice, user_choice):
-    """
-    Determine and print the winner of the game.
-
-    Args:
-        random_choice (str): The choice made by the computer.
-        user_choice (str): The choice made by the user.
-
-    Returns:
-        None
-    """
     if random_choice == user_choice:
-        print("Game draw")
+        print("Round draw")
+        return "draw"
     elif ((random_choice == 'rock' and user_choice == 'scissor') or
           (random_choice == 'paper' and user_choice == 'rock') or
           (random_choice == 'scissor' and user_choice == 'paper')):
-        print("Result: Computer won the game")
+        return "computer"
     else:
-        print("Result: You won the game")
+        return "user"
 
-def game(ask_user):
+def game(rounds):
     """
-    Run the rock-paper-scissors game loop until the user quits.
+    Runs a game of Rock-Paper-Scissors for a specified number of rounds.
+
+    This function manages the game loop, keeps track of scores for both the user
+    and the computer, and determines the winner.
 
     Args:
-        ask_user (str): The user's initial response to start the game.
+        rounds (int): The total number of rounds to be played in the game.
 
     Returns:
-        None
+        None: This function prints the game's progress, round results,
+              final scores, and the overall winner.
     """
-    while ask_user != 'quit':
+    user_score, com_score = 0, 0
+    round = 1
+    while round <= rounds:
+        print(f"\n--- Round {round}/{rounds} ---")
         choices = ['rock', 'paper', 'scissor']
         random_choice = random.choice(choices)
         user_choice = get_user_choice()
         
         if is_valid_input(user_choice, choices):
             print(f"\nYou chose: {user_choice}\nComputer chose: {random_choice}\n")
-            winner(random_choice, user_choice)
+            win = winner(random_choice, user_choice)
+            if (win == "user"):
+                user_score = user_score + 1
+            elif (win == "computer"):
+                com_score = com_score + 1
+            else:
+                user_score = user_score + 1
+                com_score = com_score + 1
+            round += 1
         else:
             print("Please enter a valid choice.")
             continue
-            
-        ask_user = input("Press any key to continue. If not, type 'quit': ").lower()
+        if round <= rounds: 
+            input("\nPress Enter to continue to the next round...")
+    
+    print(f"Your Score: {user_score}\nComputer Score: {com_score}")
+    
+    if (user_score > com_score):
+        print("You won the game\n")
+    else:
+        print("Computer won the game\n")
         
-    print("Thank you for playing!")
+    print("\nThank you for playing!")
 
 
 ask_user = input("Do you want to play the game? (yes/no): ").lower()
 if ask_user == 'yes':
-    game(ask_user)
+    rounds = int(input("How many rounds you want to play?[3 or 5] "))
+    game(rounds)
 else:
     print("No worries! See you next time.\nHave a great time!")
